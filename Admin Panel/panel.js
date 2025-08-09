fbaa214096b5590c3ef1dd1142551d12a06992e6
Parth-Pingle10 <onlynonlyminecraft@gmail.com>
2025-08-10 01:20:04 +0530

    Update panel.js

--- Admin Panel/panel.js
+++ Admin Panel/panel.js
@@ -1,35 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-document.querySelector(".search").addEventListener("input", () => {
-    const input = document.querySelector(".search").value.toLowerCase().replace(/\s*/g, "");
-    const cards = document.querySelectorAll(".card");
-    cards.forEach(card => {
-        const text = card.textContent.toLowerCase().replace(/\s*/g, "");
-        card.style.display = text.includes(input) ? "flex" : "none";
+    document.querySelector(".search").addEventListener("input", () => {
+        const input = document.querySelector(".search").value.toLowerCase().replace(/\s*/g, "");
+        const cards = document.querySelectorAll(".card");
+        cards.forEach(card => {
+            const text = card.textContent.toLowerCase().replace(/\s*/g, "");
+            card.style.display = text.includes(input) ? "flex" : "none";
+        });
     });
-});
 
-function fetchdata(){
+    function fetchdata() {
 
-    fetch(`https://drone-backend-ux0x.onrender.com/data`)
-    .then(response => response.json())
-    .then(data => {
-            const container = document.querySelector(".card-container");
-            container.innerHTML = ""
-            data.forEach(entry => {
-                const dateTime = new Date(entry.timestamp).toLocaleString("en-IN", {
-                    day: "2-digit",
-                    month: "2-digit",
-                    year: "numeric",
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    second: "2-digit",
-                    hour12: true,
-                    timeZone: "Asia/Kolkata"
-                });
+        fetch(`https://drone-backend-ux0x.onrender.com/data`)
+            .then(response => response.json())
+            .then(data => {
+                const container = document.querySelector(".card-container");
+                container.innerHTML = ""
+                data.forEach(entry => {
+                    const dateTime = new Date(entry.timestamp).toLocaleString("en-IN", {
+                        day: "2-digit",
+                        month: "2-digit",
+                        year: "numeric",
+                        hour: "2-digit",
+                        minute: "2-digit",
+                        second: "2-digit",
+                        hour12: true,
+                        timeZone: "Asia/Kolkata"
+                    });
 
-                const card = document.createElement("div")
-                card.classList.add("card")
-                card.innerHTML = `
+                    const card = document.createElement("div")
+                    card.classList.add("card")
+                    card.innerHTML = `
             <div>Altitude: ${entry.altitude}</div>
             <div>Heading: ${entry.heading}</div>
             <div>Battery: ${entry.battery}</div>
@@ -39,32 +39,32 @@ function fetchdata(){
             <div>Time: ${dateTime}</div>
             `;
 
-            container.appendChild(card);
-            })
-        });
+                    container.appendChild(card);
+                })
+            });
 
-  
+
     }
-        
+
     const saveBtn = document.querySelector(".save");
 
-    
-        saveBtn.addEventListener("click", async() => {
-            download();
-            
-            try{
-                const copy=await fetch('https://drone-backend-ux0x.onrender.com/copy',{
-                    method:"POST"
-                });
 
-                const deletedata=await fetch('https://drone-backend-ux0x.onrender.com/delete',{
-                    method:"DELETE"
-                })
-            }catch(err){
-                console.log(err);
-            }
+    saveBtn.addEventListener("click", async () => {
+        download();
 
-        });
+        try {
+            const copy = await fetch('https://drone-backend-ux0x.onrender.com/copy', {
+                method: "POST"
+            });
+
+            const deletedata = await fetch('https://drone-backend-ux0x.onrender.com/delete', {
+                method: "DELETE"
+            })
+        } catch (err) {
+            console.log(err);
+        }
+
+    });
 
 
     async function download() {
@@ -86,8 +86,8 @@ function fetchdata(){
             console.error(err);
         }
     }
+    
+    setInterval(() => {
+        fetchdata()
+    }, 1000);
 });
-
-setInterval(() => {
-   fetchdata()
-}, 1000);
