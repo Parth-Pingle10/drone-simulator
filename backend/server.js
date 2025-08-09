507524948cc13ac7bd8bff3cdf9fc74a28e16e9c
Parth-Pingle10 <onlynonlyminecraft@gmail.com>
2025-08-09 13:27:59 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/Drone', {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
